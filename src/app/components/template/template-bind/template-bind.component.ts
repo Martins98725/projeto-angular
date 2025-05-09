a5adbb360cd029dde31c510a66f57dc3c8da4f01
Martins98725 <icaro25martins@gmail.com>
2025-05-09 09:04:12 -0300

    feat: aulas sobre two way data binding e diretivas de atributos e classes condicionais

--- src/app/components/template/template-bind/template-bind.component.ts
+++ src/app/components/template/template-bind/template-bind.component.ts
@@ -1,21 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-bind',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './template-bind.component.html',
-  styleUrl: './template-bind.component.css'
+  styleUrl: './template-bind.component.css',
 })
 export class TemplateBindComponent {
-  public name = "Dener Troquatte";
+  public name = 'Dener Troquatte';
   public age = 20;
-  public condition = this.age > 1 ? "é maior" : "é menor";
+  public condition = this.age > 1 ? 'é maior' : 'é menor';
   public isDisabed = false;
   public isTextDecoration = this.age >= 20 ? 'underline' : 'none';
 
-  public sum(valor1: number, valor2: number){
-    return valor1 + valor2
+  public sum() {
+    console.log(this.age);
+    return this.age++;
   }
 
+  public sub() {
+    console.log(this.age);
+
+    return this.age--;
+  }
+
+  public onKeyDown(event: Event) {
+    return console.log(event);
+  }
+
+  public onMouseEvent(event: MouseEvent) {
+    return console.log({
+      clientx: event.clientX,
+      clientY: event.clientY,
+    });
+  }
 }
